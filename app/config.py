1fc0761d8a8e7a6d415776eecf48c84b858ec5f9
baron <baronchibuike@gmail.com>
2022-05-01 17:00:34 +0100

    fixed a bug in env file

--- app/config.py
+++ app/config.py
@@ -1,14 +1,21 @@
 from dotenv import find_dotenv, load_dotenv
 import os
+import re
 from pydantic import BaseSettings
 
 load_dotenv(find_dotenv())
+
+
+uri = os.getenv("DATABASE_URL")
+
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 class Settings(BaseSettings):
     SECRET_KEY: str = os.getenv('SECRET_KEY')
     ALGORITHM: str = os.getenv('ALGORITHM')
     ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
-    DATABASE_URL: str = os.getenv('ACTIVE_DATABASE')
+    DATABASE_URL: str = uri
 
 
 settings = Settings()

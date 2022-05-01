c8f024136794fc9bccec440875f86270dfa3364f
baron <baronchibuike@gmail.com>
2022-05-01 15:53:41 +0100

    deploying to heroku

--- app/main.py
+++ app/main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
-from .models import Base
-from .database import engine
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import post, user, auth, vote
 
-
+# when you want pydantic to create your table
 # Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:8000",
+    "https://localhost:3000",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # add all your routers
 app.include_router(post.router)

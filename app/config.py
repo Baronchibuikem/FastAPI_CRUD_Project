bb4b80f1b8c810f8e226c44cfe11e263032bb8bb
baron <baronchibuike@gmail.com>
2022-05-01 16:45:33 +0100

    fixed a bug in env file

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str = os.getenv('SECRET_KEY')
     ALGORITHM: str = os.getenv('ALGORITHM')
     ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
-    DATABASE_URL: str = os.getenv('DATABASE_URL')
+    DATABASE_URL: str = os.getenv('ACTIVE_DATABASE')
 
 
 settings = Settings()

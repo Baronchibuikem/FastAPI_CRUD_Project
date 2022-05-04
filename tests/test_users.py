588637e8d2ca0e4296eee805377368963542567c
baron <baronchibuike@gmail.com>
2022-05-04 02:53:29 +0100

    configured test database

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,4 @@
-import pytest
+from app import schemas
 
 
 def test_create_user(client):
@@ -6,14 +6,13 @@ def test_create_user(client):
         "/users/",
         json={"email": "deadpool@example.com", "password": "chimichangas4life"},
     )
-    assert response.status_code == 201, response.text
-    data = response.json()
-    assert data["email"] == "deadpool@example.com"
-    assert "id" in data
-    user_id = data["id"]
+    assert response.status_code == 201
+    data = schemas.UserResponse(**response.json())
+    assert data.email == "deadpool@example.com"
+    user_id = data.id
 
     response = client.get(f"/users/{user_id}")
     assert response.status_code == 200
-    data = response.json()
-    assert data["email"] == "deadpool@example.com"
-    assert data["id"] == user_id
+    data = schemas.UserResponse(**response.json())
+    assert data.email == "deadpool@example.com"
+    assert data.id == user_id

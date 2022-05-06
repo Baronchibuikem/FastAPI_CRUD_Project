c0ef3fde61e9b8d2af499134b6483d25dc7c4b78
baron <baronchibuike@gmail.com>
2022-05-06 14:21:27 +0100

    added test for login and incorrect login

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,8 @@ from app.config import settings
 from app.main import app
 from app.database import get_db, Base
 from fastapi.testclient import TestClient
+from app import models
+from app.utils.oauth2 import create_access_token
 
 
 SQLALCHEMY_DATABASE_URL = f'{settings.DATABASE_TYPE}://' \
@@ -40,3 +42,80 @@ def client(session):
             session.close()
     app.dependency_overrides[get_db] = override_get_db
     yield TestClient(app)
+
+
+@pytest.fixture
+def test_user(client):
+    user_data = {
+        "email": "admin@example.com",
+        "password": "admin12345"
+    }
+    res = client.post('/users/', json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+
+@pytest.fixture
+def test_second_user(client):
+    user_data = {
+        "email": "admin2@example.com",
+        "password": "admin12345"
+    }
+    res = client.post('/users/', json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id": test_user['id']})
+
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+
+    return client
+
+
+@pytest.fixture
+def test_posts(test_user, session, test_user2):
+    posts_data = [{
+        "title": "first title",
+        "content": "first content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "2nd title",
+        "content": "2nd content",
+        "owner_id": test_user['id']
+    },
+        {
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id": test_user2['id']
+    }]
+
+    def create_post_model(post):
+        return models.Post(**post)
+
+    post_map = map(create_post_model, posts_data)
+    posts = list(post_map)
+
+    session.add_all(posts)
+    # session.add_all([models.Post(title="first title", content="first content", owner_id=test_user['id']),
+    #                 models.Post(title="2nd title", content="2nd content", owner_id=test_user['id']), models.Post(title="3rd title", content="3rd content", owner_id=test_user['id'])])
+    session.commit()
+
+    posts = session.query(models.Post).all()
+    return posts
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,18 +1,32 @@
-from app import schemas
+import pytest
+from app import schemas, config
+from jose import jwt
 
 
-def test_create_user(client):
-    response = client.post(
-        "/users/",
-        json={"email": "deadpool@example.com", "password": "chimichangas4life"},
-    )
-    assert response.status_code == 201
-    data = schemas.UserResponse(**response.json())
-    assert data.email == "deadpool@example.com"
-    user_id = data.id
-
-    response = client.get(f"/users/{user_id}")
+def test_login_user(client, test_user):
+    data = {
+        'username': test_user['email'],
+        'password': test_user['password']
+    }
+    response = client.post('/login', data=data)
+    login_res = schemas.Token(**response.json())
+    payload = jwt.decode(login_res.access_token,
+                         config.settings.SECRET_KEY, algorithms=[config.settings.ALGORITHM])
+    id = payload.get("user_id")
+    assert str(id) == test_user['id']
+    assert login_res.token_type == "bearer"
     assert response.status_code == 200
-    data = schemas.UserResponse(**response.json())
-    assert data.email == "deadpool@example.com"
-    assert data.id == user_id
+
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('sanjeev@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'password123', 422),
+    ('sanjeev@gmail.com', None, 422)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post(
+        "/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
